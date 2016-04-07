274d320ab7516b168eba8c0cd80c2296082cc309
lsn : ) <chris_lsn@foxmail.com>
2016-04-08 00:09:26 +0800

    modify construction

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var log4js = require('./models/log4js'); 
 
 var app = express();
@@ -22,11 +20,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/homepage', routes);
+
+app.get('/', function (req, res) {
+  res.sendfile('app/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ app/scripts/angular/controllers.js
@@ -0,0 +1,3 @@
+function helloCtrl($scope) {
+  $scope.username = 'Lusssn';
+}
